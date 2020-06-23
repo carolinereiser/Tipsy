e16fba3f793242ab9e467820e65663d05d588c4f
Caroline Reiser <carolinereiser@fb.com>
2020-06-23 13:13:49 -0700

    Initial Commit

---
+++ Tipsy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipsy
+//
+//  Created by Caroline Reiser on 6/23/20.
+//  Copyright © 2020 Caroline Reiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipsy/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Tipsy
+//
+//  Created by Caroline Reiser on 6/23/20.
+//  Copyright © 2020 Caroline Reiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
